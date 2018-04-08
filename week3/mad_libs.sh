d2c9493742bc7757ef005d22a8a3195c4fdb8fde
Pavel Karateev <karateev.pavel@ya.ru>
2018-04-08 18:31:59 +0300

    Input

---
+++ week3/mad_libs.sh
@@ -0,0 +1,10 @@
+# Write a script that asks the user for an adjective, a noun and a verb,
+# and then use those words in sentence
+
+echo Enter an adjective:
+read adjective
+echo Enter a noun:
+read noun
+echo Enter a verb:
+read verb
+echo $adjective $noun $verb
