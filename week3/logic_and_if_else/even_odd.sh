f939f20b987ad479410749786f4137e98605b8a8
Pavel Karateev <karateev.pavel@ya.ru>
2018-04-10 22:02:52 +0300

    Fix even and odd task

--- week3/logic_and_if_else/even_odd.sh
+++ week3/logic_and_if_else/even_odd.sh
@@ -1,4 +1,4 @@
 # Write a Bash script that takes one argument and prints "even" if the first
 # argument is an even number or "odd" if the first argument is an odd number
 
-if [[ $1 % 2 -eq 0 ]]; then echo even; fi
+if [[ $1%2 -eq 0 ]]; then echo even; else echo odd; fi
